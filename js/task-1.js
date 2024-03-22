256864426e5eb6185811172e5e696246542a1be9
Annasteysha <154023562+Annasteysha@users.noreply.github.com>
2024-03-22 23:25:35 +0100

    finish-js-hw-02.js

--- js/task-1.js
+++ js/task-1.js
@@ -6,6 +6,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
      else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`
      }
+     
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
